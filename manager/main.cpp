c9d53d7a182d9aff353d461113f155398eec47df
matt <moorem27@uw.edu>
2016-08-31 20:33:24 -0700

    Test commit

--- manager/main.cpp
+++ manager/main.cpp
@@ -8,6 +8,9 @@ int main() {
     using namespace util;
 
     for ( size_t i = 0; i < MAX_MESSAGES; ++i ) {
+        // The work done by a constant force of magnitude
+        // 'F' on a point that moves a displacement 's'
+        // in the direction of the force is the product W = Fs
         std::cout << "work " << i << std::endl;
         // TODO: do work
     }
