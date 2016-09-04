87534fe4d0b674e25386e47d06ff96bfe7886ecd
matt <moorem27@uw.edu>
2016-09-04 13:54:02 -0700

    Update files in server directory

--- server/main.cpp
+++ server/main.cpp
@@ -1,92 +1,43 @@
-// @file server/main.cpp
-// @brief A datagram server demo
-
-// This example modified from original found here:
-// http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html#datagram
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <iostream>
 #include <thread>
-#include <chrono>
 
-#define MYPORT "4950"    // the port users will be connecting to
-
-#define MAXBUFLEN 100
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr( struct sockaddr *sa )
-{
-    if ( sa->sa_family == AF_INET ) {
-        return &( ( ( struct sockaddr_in*) sa )->sin_addr ); //TODO: What the hell is this?
-    }
-
-    return &( ( ( struct sockaddr_in6* )sa )->sin6_addr );   //TODO: " "?
-}
 
 int main( void ) {
     int socket_file_descriptor = 0;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    ssize_t bytes;
-    struct sockaddr_storage their_addr;
-    char buffer[ MAXBUFLEN ];
-    socklen_t addr_len;
-    char s[ INET6_ADDRSTRLEN ];
+    ssize_t bytes = 0;
+    socklen_t address_length = 0;
+    char buffer[ 1000 ];
 
-    memset( &hints, 0, sizeof hints );
-    hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    // Structure containing internet address
+    struct sockaddr_in server;
 
-    if ( ( rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo ) ) != 0 ) {
-        std::cerr << "getaddrinfo: " << gai_strerror( rv ) << std::endl;
-        return 1;
+    // Set socket_file_descriptor to value in file descriptor table
+    socket_file_descriptor = socket( AF_INET, SOCK_DGRAM, 0 );
+
+    server.sin_family = AF_INET;            // Symbolic constant AF_INET = any two hosts on internet
+    server.sin_port = htons( 5555 );        // Use htons to convert from host byte order to network byte order
+    server.sin_addr.s_addr = INADDR_ANY;    // Symbolic constant INADDR_ANY = server IP address
+
+    // Bind socket to address
+    if( bind( socket_file_descriptor, (struct sockaddr*)&server, sizeof( server ) ) < 0 ) {
+        std::cerr << "Did not bind successfully" << std::endl;
     }
 
-    // loop through all the results and bind to the first we can
-    for ( p = servinfo; p != NULL; p = p->ai_next ) {
-        if ( ( socket_file_descriptor = socket( p->ai_family, p->ai_socktype,
-                             p->ai_protocol ) ) == -1 ) {
-            perror( "listener: socket" );
-            continue;
-        }
-
-        if ( bind( socket_file_descriptor, p->ai_addr, p->ai_addrlen ) == -1 ) {
-            close( socket_file_descriptor );
-            perror( "listener: bind" );
-            continue;
-        }
-
-        break;
-    }
-
-    if ( p == NULL ) {
-        fprintf( stderr, "listener: failed to bind socket\n" );
-        return 2;
-    }
-
-    freeaddrinfo( servinfo );
-
+    // (In a separate terminal enter 'bmon' to monitor network traffic)
     std::cout << "Listener is waiting" << std::endl;
 
-    addr_len = sizeof their_addr;
+    address_length = sizeof( server );
     int count = 0;
     while( ++count < 1000 ) {
-        if ( ( bytes = recvfrom( socket_file_descriptor, buffer, MAXBUFLEN - 1, 0,
-                              (struct sockaddr *) &their_addr, &addr_len ) ) == -1 ) {
-            perror("recvfrom");
-            exit(1);
+        bytes = recvfrom( socket_file_descriptor, buffer, sizeof( buffer ), 0, (struct sockaddr*)&server, &address_length );
+        if( bytes < 0 ) {
+            std::cerr << "Receive failed" << std::endl;
         }
-        std::cout << "listener: got packet from " << inet_ntop( their_addr.ss_family,
-                                                               get_in_addr( ( struct sockaddr * ) &their_addr ),
-                                                               s, sizeof s ) << std::endl;
         std::cout << "listener: packet is " << bytes << " bytes long" << std::endl;
         buffer[ bytes ] = '\0';
         std::cout << count << " " << buffer << std::endl;
@@ -98,4 +49,3 @@ int main( void ) {
 
     return 0;
 }
-
