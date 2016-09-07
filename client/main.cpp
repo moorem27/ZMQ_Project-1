8fc176f5c47ae3329ca4c64e154c83c11f6d7fc4
matt <moorem27@uw.edu>
2016-09-06 20:39:11 -0700

    Separate logic into separate functions

--- client/main.cpp
+++ client/main.cpp
@@ -7,6 +7,8 @@
 #include <arpa/inet.h>
 #include <zmq.hpp>
 
+constexpr int MAX_MESSAGE_SIZE  = 65507;
+
 bool complete_handshake() {
     bool handshake_complete = false;
     // ----------------- For ZMQ socket -------------------------
@@ -30,42 +32,53 @@ bool complete_handshake() {
     return handshake_complete;
 }
 
-//TODO: Begin breaking this apart into functions
-int main( void )
-{
-    bool handshake_complete = complete_handshake();
 
-    // ------------------ For UDP socket -----------------------
-    int socket_file_descriptor = 0;
-
-    // Structure containing internet address
-    struct sockaddr_in client;
-
-    socket_file_descriptor = socket( AF_INET, SOCK_DGRAM, 0 );
+void initialize_udp_socket( int& socket_file_descriptor, sockaddr_in& client ) {
+    socket_file_descriptor  = socket( AF_INET, SOCK_DGRAM, 0 );
     memset( &client, 0, sizeof( client ) );
 
-    client.sin_family = AF_INET;
-    client.sin_port = htons( 4950 );
-    client.sin_addr.s_addr = inet_addr( "127.0.0.1" );
-    // ----------------------------------------------------------
+    client.sin_family       = AF_INET;
+    client.sin_port         = htons( 4950 );
+    client.sin_addr.s_addr  = inet_addr( "127.0.0.1" );
+}
+
+
+void send_messages( const int socket_file_descriptor, const sockaddr_in& client, std::string message ) {
+    size_t message_size = strlen( message.c_str() );
+
+    if( message_size < MAX_MESSAGE_SIZE ) {
+        if ( sendto( socket_file_descriptor, message.c_str(), message_size, 0,
+                     (struct sockaddr *) &client, sizeof( client ) ) == -1 ) {
+            perror( "Send failed" );
+            exit( 1 );
+        }
+        std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
+    } else {
+        std::cerr << "Message size exceeds limit" << std::endl;
+    }
+}
+
+
+int main( void ) {
+    int socket_file_descriptor = 0;
+    struct sockaddr_in client{};
+    std::string message{};
+
+    bool handshake_complete = complete_handshake();
 
     if( handshake_complete ) {
-        std::cout << "Handshake complete" << std::endl;
-        int count = 0;
-        while ( ++count < 100 ) {
-            std::string message = "All types of pizza are welcome.";
+        initialize_udp_socket( socket_file_descriptor, client );
 
-            if ( sendto( socket_file_descriptor, message.c_str(), sizeof( message ), 0,
-                       (struct sockaddr *) &client, sizeof( client ) ) == -1) {
-                perror( "Send failed" );
-                exit( 1 );
-            }
-            std::cout << count << " Client sent: " << message << std::endl;
-            std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
-       }
-        close( socket_file_descriptor );
+        // TODO: Add condition to while loop
+        while( 1 ) {
+            // Block for message, signal, etc
+            std::cout << "Enter a message: ";
+            std::getline( std::cin, message );
+
+            send_messages( socket_file_descriptor, client, message );
+        }
     }
 
-
+    close( socket_file_descriptor );
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -7,6 +7,11 @@
 #include <thread>
 #include <zmq.hpp>
 
+
+constexpr int UDP_PORT_NUMBER   = 4950;
+constexpr int BUFFER_LIMIT      = 1000;
+
+
 void respond_to_handshake() {
     zmq::context_t context( 1 );
     zmq::socket_t zmq_socket( context, ZMQ_REP );
@@ -24,50 +29,56 @@ void respond_to_handshake() {
     zmq_socket.send( reply );
 }
 
-//TODO: Begin breaking this apart into functions
-int main( void ) {
 
-    respond_to_handshake();
+void initialize_udp_socket( int& socket_file_descriptor, sockaddr_in& server ) {
+    socket_file_descriptor  = socket( AF_INET, SOCK_DGRAM, 0 );
 
-    // ----------------------- For UDP socket -------------------------
-    int socket_file_descriptor = 0;
-    ssize_t bytes = 0;
-    socklen_t address_length = 0;
-    char buffer[ 1000 ];
-
-    // Structure containing internet address
-    struct sockaddr_in server;
-
-    // Set socket_file_descriptor to value in file descriptor table
-    socket_file_descriptor = socket( AF_INET, SOCK_DGRAM, 0 );
-
-    server.sin_family = AF_INET;            // Symbolic constant AF_INET = any two hosts on internet
-    server.sin_port = htons( 4950 );        // Use htons to convert from host byte order to network byte order
-    server.sin_addr.s_addr = INADDR_ANY;    // Symbolic constant INADDR_ANY = server IP address
+    server.sin_family       = AF_INET;            // Symbolic constant AF_INET = any two hosts on internet
+    server.sin_port         = htons( UDP_PORT_NUMBER );        // Use htons to convert from host byte order to network byte order
+    server.sin_addr.s_addr  = INADDR_ANY;    // Symbolic constant INADDR_ANY = server IP address
 
     // Bind socket to address
     if( bind( socket_file_descriptor, (struct sockaddr*)&server, sizeof( server ) ) < 0 ) {
         std::cerr << "Did not bind successfully" << std::endl;
     }
-    // ----------------------------------------------------------------
+}
+
+
+void receive_messages( const int socket_file_descriptor, sockaddr_in& server ) {
+    ssize_t bytes                   = 0;
+    socklen_t address_length        = 0;
+    char buffer[ BUFFER_LIMIT ];
 
     std::cout << "Server is waiting" << std::endl;
 
-    address_length = sizeof( server );
-    int count = 0;
-    while( ++count < 1000 ) {
+    address_length  = sizeof( server );
+
+    // TODO: Add condition to while loop
+    while( 1 ) {
         bytes = recvfrom( socket_file_descriptor, buffer,
                           sizeof( buffer ), 0, (struct sockaddr*)&server,
                           &address_length );
         if( bytes < 0 ) {
             std::cerr << "Receive failed" << std::endl;
         }
-        buffer[ bytes ] = '\0';
-        std::cout << count << " " << buffer << std::endl;
+        std::cout << "Received: " << std::string{ buffer }<< std::endl;
         std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
+
+        memset( &buffer, 0, BUFFER_LIMIT );
     }
 
-    close( socket_file_descriptor) ;
+}
 
+
+int main( void ) {
+    int socket_file_descriptor = 0;
+    struct sockaddr_in server{};
+
+    initialize_udp_socket( socket_file_descriptor, server );
+
+    respond_to_handshake();
+    receive_messages( socket_file_descriptor, server );
+
+    close( socket_file_descriptor) ;
     return 0;
 }
